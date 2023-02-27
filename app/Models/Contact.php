87df8a08ae140f001cc7fa200626b65044dfdfe7
Hezron-tech <kiprophez@gmail.com>
2023-02-27 10:19:08 +0300

    adding the index page

---
+++ app/Models/Contact.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+
+    protected $table ='contacts';
+    protected $primaryKey ='id';
+    protected $filllable =['name','address','mobile'];
+    
+}
