5e506d8de80dd6ac234df076be7298cf965a9fdc
Hezron-tech <kiprophez@gmail.com>
2023-02-27 10:30:40 +0300

    edit page

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,25 @@
+@extends('contacts.layout')
+@section('content')
+
+<div class="card">
+<div class="card-header"> Contact us page</div>
+
+<div ="card-body">
+
+<form action="{{url ('contact'}}" method="post">
+{!! csrf_field() !!}
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Address</label></br>
+        <input type="text" name="address" id="address" class="form-control"></br>
+        <label>Mobile</label></br>
+        <input type="text" name="mobile" id="mobile" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+
+
+</form>
+
+</div>
+
+</div>
+@stop
