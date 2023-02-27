57f028df7c2076da23103c5be86d69e47929ad16
Hezron-tech <kiprophez@gmail.com>
2023-02-27 10:47:54 +0300

    working on controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,84 +1,69 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
+        $contacts = Contact::all();
+        return view ('contacts.index')->with('contacts', $contacts);
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
+        return view('contacts.create');
+
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
+
+        $input = $request->all();
+        Contact::create($input);
+        return redirect ('contact')->with('flash_message', 'Contact Added');
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
+        $contact = Contact::find($id);
+        return view('contacts.show')->with('contacts' , $contact);
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
+        $contact = Contact::find($id);
+        return view('contacts.edit')->with('contacts', $contact);
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, $id)
     {
+
+        $contact = Contact::find($id);
+        $input = $request->all();
+        $contact->update($input);
+        return redirect('contact')->with ('flash_message', 'Contact updated');
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy($id)
     {
+        Contact::destroy($id);
+        return redirect('contact')->with('flash_messagge','Contact deleted');
         //
     }
 }
